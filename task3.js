f92cea8396b17dc4a907b8b39ff2d778df4da86d
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2024-11-03 14:36:42 +0300

    all documentes are formatted

--- task3.js
+++ task3.js
@@ -1,6 +1,9 @@
-function getElementWidth(content,padding,border){
-    const totalWidth=Number.parseFloat(content)+Number.parseFloat(padding)*2+Number.parseFloat(border)*2;
-    return totalWidth;
+function getElementWidth(content, padding, border) {
+  const totalWidth =
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2;
+  return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
